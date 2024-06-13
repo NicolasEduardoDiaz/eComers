5982f6100859b18dde28fe3fdb133496032e79f0
NicolasEduardoDiaz <160768951+NicolasEduardoDiaz@users.noreply.github.com>
2024-06-13 07:42:57 -0500

    feat: :sparkles: agregando contenido a el archvio main.js

--- js/main.js
+++ js/main.js
@@ -1,21 +1,22 @@
 //let header_information = document.queriSelector(".header_information")
 import { menulistCategoryIndex } from "./module/menu.js";
 import { galleryindex } from "./components/gallery.js";
-import { getAllProductName } from "./modules/app.js";
+import { getAllCategory, getAllProductName } from "./modules/app.js";
 
 let input_search = document.querySelector("#input_search");
 let main_article = document.querySelector(".main_article");
 let nav_ul = document.querySelector(".nav_ul");
 
 addEventListener("DOMContentLoaded", async (e) => {
-    let data = await getAllCategory();
-    nav_ul.innerHTML = await menulistCategoryIndex(data);
+    if(!localStorage.getItem("getAllCategory")) localStorage.setItem("getAllCategory", JSON.stringify(await getAllCategory()));
+    nav_ul.innerHTML = await menulistCategoryIndex(JSON.parse(localStorage.getItem("getAllCategory")));
 })
 
 input_search.addEventListener("change", async e => {
+    let params = new URLSearchParams(location.search);
     let data = {search : e.target.value}
     input_search.value = null;
     
     let res = await getAllProductName(data)
-    main_article.innerHTML += galleryindex(res);
+    main_article.innerHTML += galleryindex(res, params.get('id'));
 });
